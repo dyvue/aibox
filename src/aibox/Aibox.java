fcf776a5efe7b45ad185c065c725081e45ca0c66
Romadhan Edy Prasetyo <romadhanedy@gmail.com>
2022-06-26 01:13:51 +0700

    init project

---
+++ src/aibox/Aibox.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package aibox;
+
+import aibox.view.Layout;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author romad
+ */
+public class Aibox extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Layout ui = new Layout();
+        ui.setVisible(true);   
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
